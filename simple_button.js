71ab65d52da86a7534dcab5ed4c8e33a9d1686f6
Mihail Zhizhikin <mihailzizikin330@gmail.com>
2022-08-02 14:55:14 +0500

    Added few styles for button (becomes darker when mouse cursor hover it) and renamed the div ('main', not 'test')

--- simple_button.js
+++ simple_button.js
@@ -5,7 +5,10 @@ const e = React.createElement;
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = { 
+      liked: false,
+      text:'Hello'
+     };
   }
 
   render() {
@@ -16,7 +19,7 @@ class LikeButton extends React.Component {
     return e(
       'button',
       { class: "simple-button", onClick: () => this.setState({ liked: true }) },
-      'Like'
+      this.state.text
     );
   }
 }
