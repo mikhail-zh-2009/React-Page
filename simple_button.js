9aa62722a4db5b13bd404d8506a9f67306789c08
Mihail Zhizhikin <mihailzizikin330@gmail.com>
2022-08-02 14:48:59 +0500

    Added styles for simple button class

---
+++ simple_button.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { class: "simple-button", onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+const domContainer = document.querySelector('#test');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
