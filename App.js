527f2448ddc764e12d6c82ec360206bf70106124
Mihail Zhizhikin <mihailzizikin330@gmail.com>
2022-08-03 14:59:42 +0500

    Added Subscribe and Like button and their classes

--- App.js
+++ App.js
@@ -2,11 +2,71 @@ function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
 }
 
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isToggleOn: true,
+            onText: '',
+            offText: '',
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    clickEvent() {}
+
+    handleClick() {
+        this.setState(prevState => ({
+        isToggleOn: !prevState.isToggleOn
+        }));
+        this.clickEvent();
+    }
+
+    render() {
+        return (
+        <button class="simple-button" onClick={this.handleClick}>
+            {this.state.isToggleOn ? this.state.offText : this.state.onText}
+        </button>
+        );
+    }
+}
+
+class LikeButton extends Button {
+    constructor(props) {
+        super(props);
+        this.state = {
+            onText: 'Like',
+            offText: 'Unlike'
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    clickEvent() {
+        
+    }
+}
+
+class SubscribeButton extends Button {
+    constructor(props) {
+        super(props);
+        this.state = {
+            onText: 'Subscribe',
+            offText: 'Unsubscribe',
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    clickEvent() {
+        
+    }
+}
+
+function Print() {
+    document.write("1")
+}
+
 function App() {
     return <div>
-        <Welcome name="Anatoliy"/>
+        <LikeButton/> <SubscribeButton/>
     </div>
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = <App/>;
-root.render(element);
+root.render(<App/>)
