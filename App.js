37da62dba28e16ed6e68c6c67eb8c7a1e700e957
Mihail Zhizhikin <mihailzizikin330@gmail.com>
2022-08-03 16:14:02 +0500

    Added Number Button Class

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// Button main class
+var text = ' ';
+var splitted = text.split(' ');
+
+function Equals() {
+    var result = Number(splitted[0])
+    for(var i in splitted) {
+        if(splitted[i] == '+') {
+            result += Number(splitted[i + 1]);
+        }
+    }
+    text = result;
+}
+
+function CheckText() {
+    if(splitted[splitted.length] == '=') {
+        Equals();
+    }
 }
 
 class Button extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: ''
+            text: '',
         };
         this.handleClick = this.handleClick.bind(this);
     }
 
-    clickEvent() {}
+    // Click event that will be executed when button clicked
+    clickEvent() {
+        root.render(<App />)
+        text += this.props.text
+    }
 
     handleClick() {
         this.clickEvent();
@@ -20,33 +41,38 @@ class Button extends React.Component {
     render() {
         return (
         <button class="simple-button" onClick={this.handleClick}>
-            {this.state.text}
+            {this.props.text}
         </button>
         );
     }
 }
 
+// Like button
 class NumberButton extends Button {
     constructor(props) {
         super(props);
         this.state = {
-            text: '',
+            text: '1',
         };
         this.handleClick = this.handleClick.bind(this);
     }
-    clickEvent() {
-        
-    }
 }
 
-function Print() {
-    document.write("1")
+function DrawText(props) {
+    return <div class="simple-text-field">{props.text}</div>;
 }
+
 
 function App() {
     return <div>
-        <NumberButton state={{text:"q"}}/>
+        <DrawText text={text} /><br/>
+        <NumberButton text="1"/><NumberButton text="2"/><NumberButton text="3"/><NumberButton text=" + "/>
+        <NumberButton text="4"/><NumberButton text="5"/><NumberButton text="6"/><NumberButton text=" - "/>
+        <NumberButton text="7"/><NumberButton text="8"/><NumberButton text="9"/><NumberButton text=" / "/>
+        <NumberButton text="."/><NumberButton text="0"/><NumberButton text=" = "/><NumberButton text=" X "/>
     </div>
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+setInterval(CheckText, 1);
+
 root.render(<App/>)
