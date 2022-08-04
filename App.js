383e2246a30657ddf04f137b2264dfc51d85a9f6
Mihail Zhizhikin <mihailzizikin330@gmail.com>
2022-08-04 13:01:53 +0500

    End.

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+// Text of upper field
 var fieldText = ' '
 
-function Equals() {
-    root.render(<App/>)
+// This function checks the expression length and cuts it if length too big.
+function Check() {
     var text = fieldText.trim().split(' ')
+    if(text.length > 3) {
+        fieldText = ' ' + text[0] + ' ' + text[1] + ' '
+    }
+}
+
+setInterval(Check, 100);
+
+
+// This function calls when user presses '='
+function Equals(source) {
+    root.render(<App/>)
+    var text = source.trim().split(' ')
     var result = Number(text[0])
     for(var i = 0; i < text.length; i++) {
         if(text[i] == '+')
             result += Number(text[i + 1])
         if(text[i] == '-')
             result -= Number(text[i + 1])
+        if(text[i] == 'X')
+            result *= Number(text[i + 1])
+        if(text[i] == '/')
+            result /= Number(text[i + 1])
+        if(text[i] == '^')
+            result **= Number(text[i + 1])
     }
     fieldText = result
 }
 
-// Button main class for "calculator"
+// Button main class
 class Button extends React.Component {
     constructor(props) {
         super(props)
@@ -30,8 +49,10 @@ class Button extends React.Component {
             fieldText += this.props.text
         else if(this.props.type == 'clear_all')
             fieldText = ''
+        else if(this.props.type == 'remove')
+            fieldText = fieldText.slice(0, fieldText.length - 1)
         else if(this.props.type == 'equals')
-            Equals()
+            Equals(fieldText)
     }
 
     render() {
@@ -43,6 +64,7 @@ class Button extends React.Component {
     }
 }
 
+// Function that draw text field
 function DrawText(props) {
     return <div className="simple-text-field">{props.text}</div>
 }
@@ -52,9 +74,9 @@ function App() {
     return <div>
         <DrawText text={fieldText} />
         <Button text="1"/><Button text="2"/><Button text="3"/><Button text=" + "/><Button text="C" type="clear_all"/>
-        <Button text="4"/><Button text="5"/><Button text="6"/><Button text=" - "/>
-        <Button text="7"/><Button text="8"/><Button text="9"/><Button text=" / "/>
-        <Button text="."/><Button text="0"/><Button text=" = " type="equals"/><Button text=" X "/>
+        <Button text="4"/><Button text="5"/><Button text="6"/><Button text=" - "/><Button text="<" type="remove"/>
+        <Button text="7"/><Button text="8"/><Button text="9"/><Button text=" / "/><Button text="=" type="equals"/>
+        <Button text="."/><Button text="0"/><Button text="000"/><Button text=" X "/><Button text=" ^ "/>
     </div>
 }
 
